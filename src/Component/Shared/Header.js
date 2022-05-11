c537c0b584e96ef85baf12a030c22bc4ed26c2cb
mdriyadmr968 <mdriyadmr968@gmail.com>
2022-05-12 01:05:34 +0600

    first commit

---
+++ src/Component/Shared/Header.js
@@ -0,0 +1,64 @@
+import { getAuth, signOut } from "firebase/auth";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import app from "../../firebase.init";
+import "./Header.css";
+
+const auth = getAuth(app);
+
+const Header = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+  return (
+    <div>
+      <header class="header">
+        <a href="" class="logo">
+          Dream World
+        </a>
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn">
+          <span class="navicon"></span>
+        </label>
+        <ul class="menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <a href="">
+              {user?.uid ? (
+                <button
+                  type="button"
+                  class="btn btn-warning"
+                  onClick={() => signOut(auth)}
+                >
+                  sign out
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  class="btn btn-warning"
+                  onClick={() => navigate("/login")}
+                >
+                  Login
+                </button>
+              )}
+            </a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2Hi69_Nghv1HHLBAnGBAaSWhO41hCPKE",
+  authDomain: "dream-world-892da.firebaseapp.com",
+  projectId: "dream-world-892da",
+  storageBucket: "dream-world-892da.appspot.com",
+  messagingSenderId: "113028391154",
+  appId: "1:113028391154:web:324a6d8d23222d5e4f706b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
